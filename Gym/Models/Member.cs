4e65322678cee714fb16473903b99c9044e7ad8c
Mitchell Gantz <mgantz22@icloud.com>
2023-03-15 09:56:22 -0700

    update member model

--- Gym/Models/Member.cs
+++ Gym/Models/Member.cs
@@ -6,6 +6,7 @@ namespace Gym.Models
     public class Member
     {
         public int MemberId {get; set;}
+        public string MemberName {get; set;}
         public int ClassId {get; set;}
         public Class Class {get; set;}
         
