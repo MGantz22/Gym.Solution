1fe7dcfaed370e01c704d4a99881c9ce38584b7a
Mitchell Gantz <mgantz22@icloud.com>
2023-03-15 10:33:07 -0700

    up date class model Co-authored-by: Carl Sustrich <CarlSustrich@users.noreply.github.com> Co-authored-by: John Lenz <john.lenz27@gmail.com>

--- Gym/Models/Class.cs
+++ Gym/Models/Class.cs
@@ -7,6 +7,8 @@ public class Class
   public int ClassId { get; set; }
   public string ClassName { get; set; }
   public int InstructorId {get; set;}
+  // date
+  // max members
   public Instructor Instuctor {get; set;}
   public int LocationId {get; set;}
   public Location Location {get; set;}
