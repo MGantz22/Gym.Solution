34df121075988173d6b8726cc5b6090182f495a3
Mitchell Gantz <mgantz22@icloud.com>
2023-03-15 10:11:41 -0700

    add location instructor model

---
+++ Gym/Models/LocationInstructor.cs
@@ -0,0 +1,11 @@
+namespace Gym.Models
+{
+    public class LocationInstructor
+    {
+        public int LocationInstructorId {get; set;}
+        public int LocationId {get; set;}
+        public int InstructorId {get; set;}
+        public Location Location {get; set;}
+        public Instructor Instructor {get; set;}
+    }
+}
