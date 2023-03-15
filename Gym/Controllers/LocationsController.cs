1a8d699d9ece7248be40e7672c02c97f01e80ee3
Mitchell Gantz <mgantz22@icloud.com>
2023-03-15 14:17:15 -0700

    update locationsController

--- Gym/Controllers/LocationsController.cs
+++ Gym/Controllers/LocationsController.cs
@@ -1,9 +1,35 @@
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.Primitives;
 using Microsoft.AspNetCore.Mvc;
 using Gym.Models;
 using System.Collections.Generic;
 using System.Linq;
-
 namespace Gym.Controllers;
+
+public class LocationsController : Controller
+{
+  private readonly GymContext _db;
+
+  public LocationsController(GymContext db)
+  {
+      _db = db;
+  }
+
+  public ActionResult Index()
+  {
+    return View(_db.Locations.ToList());
+  }
+
+  public ActionResult Create()
+  {
+    return View();
+  }
+
+  [HttpPost]
+  public ActionResult Create(Location newLocation)
+  {
+    _db.Locations.Add(newLocation);
+    _db.SaveChanges();
+    return RedirectToAction("Index");
+  }
+}
