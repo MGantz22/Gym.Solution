947dad105245cf8e03f108cc3493adc8c4b33805
Mitchell Gantz <mgantz22@icloud.com>
2023-03-16 10:39:33 -0700

    delete typeof class

--- Gym/Models/GymContext.cs
+++ Gym/Models/GymContext.cs
@@ -8,7 +8,6 @@ public class GymContext : DbContext
   public DbSet<Location> Locations { get; set; }
   public DbSet<Member> Members { get; set; }
   public DbSet<Instructor> Instructors {get; set;}
-  public DbSet<TypeOfClass> ClassTypes {get; set;} 
   public DbSet<ClassMember> ClassMembers { get; set; }
   public DbSet<LocationInstructor> LocationInstructors {get; set;}
   
