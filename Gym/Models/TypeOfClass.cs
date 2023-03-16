13dd3b8822eecabf2266256a2a82879865525522
Mitchell Gantz <mgantz22@icloud.com>
2023-03-16 10:38:03 -0700

    Delete TypeOfClass

--- Gym/Models/TypeOfClass.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections.Generic;
-namespace Gym.Models;
-
-public class TypeOfClass 
-{
-    public int TypeOfClassId {get;set;}
-    public string TypeOfClassName {get; set;}
-    public List<Class> Classes {get;set;}
-}
