793c763d9c46b4120d6183bee3046b4756a311b8
Mitchell Gantz <mgantz22@icloud.com>
2023-03-16 10:37:05 -0700

    add new migrations

---
+++ Gym/Migrations/20230316162753_seehowitworks.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Gym.Migrations
+{
+    public partial class seehowitworks : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Classes_ClassTypes_TypeOfClassId",
+                table: "Classes");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "TypeOfClassId",
+                table: "Classes",
+                type: "int",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldNullable: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Classes_ClassTypes_TypeOfClassId",
+                table: "Classes",
+                column: "TypeOfClassId",
+                principalTable: "ClassTypes",
+                principalColumn: "TypeOfClassId",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Classes_ClassTypes_TypeOfClassId",
+                table: "Classes");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "TypeOfClassId",
+                table: "Classes",
+                type: "int",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Classes_ClassTypes_TypeOfClassId",
+                table: "Classes",
+                column: "TypeOfClassId",
+                principalTable: "ClassTypes",
+                principalColumn: "TypeOfClassId");
+        }
+    }
+}
---
+++ Gym/Migrations/20230316164136_byeeeeeeeee.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Gym.Migrations
+{
+    public partial class byeeeeeeeee : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Classes_ClassTypes_TypeOfClassId",
+                table: "Classes");
+
+            migrationBuilder.DropTable(
+                name: "ClassTypes");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Classes_TypeOfClassId",
+                table: "Classes");
+
+            migrationBuilder.DropColumn(
+                name: "TypeOfClassId",
+                table: "Classes");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "TypeOfClassId",
+                table: "Classes",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.CreateTable(
+                name: "ClassTypes",
+                columns: table => new
+                {
+                    TypeOfClassId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    TypeOfClassName = table.Column<string>(type: "longtext", nullable: true)
+                        .Annotation("MySql:CharSet", "utf8mb4")
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ClassTypes", x => x.TypeOfClassId);
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Classes_TypeOfClassId",
+                table: "Classes",
+                column: "TypeOfClassId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Classes_ClassTypes_TypeOfClassId",
+                table: "Classes",
+                column: "TypeOfClassId",
+                principalTable: "ClassTypes",
+                principalColumn: "TypeOfClassId",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
