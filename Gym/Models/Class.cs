6020143c933874189747b495ac29b1b4ac1eb833
Mitchell Gantz <mgantz22@icloud.com>
2023-03-16 10:38:59 -0700

    delete typeofclass

--- Gym/Models/Class.cs
+++ Gym/Models/Class.cs
@@ -8,12 +8,10 @@ public class Class
   public string Name { get; set; }
   // date
   // max members
-  public int TypeOfClassId {get;set;}
-  public TypeOfClass ClassType {get;set;}
   public int InstructorId {get; set;}
   public Instructor Instructor {get; set;}
   public int LocationId {get; set;}
   public Location Location {get; set;}
   public List<ClassMember> JoinEntities {get;}
-  public LocationInstructor placetimething {get;set;}
+  // public LocationInstructor placetimething {get;set;}
 }
